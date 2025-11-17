66879243a2c324607458b6e8fac8cc4a5b6de9fb
Mcreviston <mcreviston2@ivytech.edu>
2025-11-16 20:59:54 -0500

    Add files via upload

---
+++ my first Python program.py
@@ -0,0 +1,9 @@
+Python 3.14.0 (v3.14.0:ebf955df7a8, Oct  7 2025, 08:20:14) [Clang 16.0.0 (clang-1600.0.26.6)] on darwin
+Enter "help" below or click "Help" above for more information.
+>>> Print("Hello World")
+Traceback (most recent call last):
+  File "<pyshell#0>", line 1, in <module>
+    Print("Hello World")
+NameError: name 'Print' is not defined. Did you mean: 'print'?
+>>> print("Hello World")
+Hello World
